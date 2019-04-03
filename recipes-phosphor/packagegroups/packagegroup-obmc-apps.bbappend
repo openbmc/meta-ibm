openbmc/meta-ibm

commit 4b2c10ea8e3bf957ea43ccc2957eaf3369d69f96

meta-ibm: react to phosphor-rest removal
phosphor-rest-server and phosphor-gevent are no longer the project
defaults so no need to expliclty remove it any longer.

Change-Id: Ib746f75bb489c01bc3c2807332cc44a76676c00d
Signed-off-by: Brad Bishop <bradleyb@fuzziesquirrel.com>

recipes-phosphor/packagegroups/packagegroup-obmc-apps.bbappend
@@ -1,2 +1,0 @@
-RDEPENDS_${PN}-logging += "ibm-logging"
-RDEPENDS_${PN}-remove += " phosphor-rest phosphor-gevent"

commit 2e039689ba4752ccece3f2811b6b2351195d7b5d

meta-ibm: Add ibm-logging back in
ibm-logging was mistakenly removed.

Tested: built WS image and ibm-log-manager found in rootfs
Fixes: 4b2c10ea8e
Change-Id: Ife473a6ce412d604c3b35bd400793c21a1d932c8
Signed-off-by: Brad Bishop <bradleyb@fuzziesquirrel.com>

recipes-phosphor/packagegroups/packagegroup-obmc-apps.bbappend
@@ -1,0 +1,1 @@
+RDEPENDS_${PN}-logging += "ibm-logging"